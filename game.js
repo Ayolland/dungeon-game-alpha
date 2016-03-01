aecbe378bfb2da0e22f203d42daf591859602d83
Andrew Y <and.t.yoll@gmail.com>
2016-03-01 13:46:08 -0600

    made log multi-line, added Monster HP

--- game.js
+++ game.js
@@ -1,7 +1,17 @@
 
 // Global Variables
-var gameLog = document.getElementById('gameLog');
-var currentMonster = new Monster;
+var gameLog = {
+	element: document.getElementById('gameLog'),
+	add: function(message){
+		this.element.innerHTML += (message + '<br />');
+		this.element.scrollTop = this.element.scrollHeight;
+	}
+}
+var headsUpMonsterName = document.getElementById('monster-name');
+var headsUpMonsterHPText = document.getElementById('monster-hp-text');
+var headsUpMonsterHPBar = document.getElementById('monster-hp-bar');
+
+var currentMonster = new Monster();
 var previousMonsterName = "";
 
 function Displayable (){
@@ -25,6 +35,7 @@ Displayable.prototype.draw = function(){
 
 function Monster(){
 	this.canvas = document.getElementById('monster-sprite').getContext('2d');
+	this.shortName = ""
 }
 
 Monster.prototype = new Displayable ();
@@ -40,17 +51,31 @@ Monster.prototype.announce = function(){
 		article = 'a ';
 	}
 	var message = "It's " + article + this.displayName + '!';
-	gameLog.innerHTML = message;
+	gameLog.add(message);
+	if (this.shortName !== ""){
+		headsUpMonsterName.innerHTML = this.shortName;
+	} else {
+		headsUpMonsterName.innerHTML = this.displayName;
+	}
+	
+};
+
+Monster.prototype.updateHP = function(){
+	headsUpMonsterHPText.innerHTML = this.HP + "/" + this.initialHP;
+	headsUpMonsterHPBar.style.width = Math.floor((this.HP / this.initialHP)*100) + "%";
 };
 
 Monster.prototype.appear = function(){
 	this.announce();
 	this.draw();
+	this.HP = this.initialHP;
+	this.updateHP();
 };
 
 // Types of Monsters
 
 function Axedude (type) {
+	this.initialHP = 20;
 	this.spriteCompressed = "16x48|000000000000001E085E078A05081FE83FE83FF81FDC07880F880FC01FE03DD018C818601860102030300000000000000000000000000000001E085E078A05081FE83FE83FF81FDC07880F880FC01FE03DD018E8186010203030000000000000";
 	this.displayName = "Axedude";
 	this.color = 'yellow';
@@ -59,6 +84,7 @@ Axedude.prototype = new Monster();
 Axedude.prototype.constructor = Axedude;
 
 function Balltype (type) {
+	this.initialHP = 15;
 	this.spriteCompressed = "16x48|0000000000000000000003800FE01FF03D783AB87C7C7ABC6D742FF427E405A001200100010000000000000000000000000000000000000003800FE01FF03FF83EF87D7C7EFC6FF42FF427E405A0012001000100000000000000000000000000";
 	this.displayName = "Gooball";
 	this.color = 'purple';
@@ -67,6 +93,7 @@ Balltype.prototype = new Monster();
 Balltype.prototype.constructor = Balltype;
 
 function Scamp (type) {
+	this.initialHP = 10;
 	this.spriteCompressed = "16x48|000000000000000000000300018003C007E0018007E00FF013C803C007E0076006200820000000000000000000000000000000000000000000000600030007800FC003000FC01FE0279003C007E0076006200820000000000000000000000000";
 	this.displayName = "Scamp";
 	this.color = 'darkgreen';
@@ -75,12 +102,14 @@ Scamp.prototype = new Monster();
 Scamp.prototype.constructor = Scamp;
 
 function Skele (type) {
+	this.initialHP = 12;
 	if ( type === ""){
-		type = randomEntry(["Footman","Archer","Monk","Knight","Flaming"])
-	};
+		type = randomEntry(["Footman","Archer","Monk","Knight","Flaming"]);
+	}
 	this.color = 'white';
 	switch (type){
 		case "Footman":
+			this.initialHP = 14;
 			this.spriteCompressed = "16x48|00000000000000000100810082C0C7E045302BD4280E118E13CE02440240042004200420041000000000000000000000000000000000000000804080416063F0229815EA140708C709E701220220022004200410041";
 			this.displayName = "Skelebones Footman";
 			break;
@@ -91,15 +120,19 @@ function Skele (type) {
 		case "Monk":
 			this.spriteCompressed = "16x48|00000000000000000100810082C047E045302BD02808118813C80A400A40062004200420041000000000000000000000000000000000000000804080416023F0229815E8140408C409E405200720022004200410041";
 			this.displayName = "Wise, Old Skelebones";
+			this.shortName = "Skelebones Mage"
 			break;
 		case "Knight":
+			this.initialHP = 16;
 			this.spriteCompressed = "16x48|00000000000004400380810082C0C7E045302BD4280E118E13CE02440240042004200420041000000000000000000000000000000000022001C04080416063F0229815EA140708C709E701220220022004200410041";
 			this.displayName = "Skelebones who thinks he's a badass";
+			this.shortName = "Skelebones Bruiser"
 			break;
 		case "Flaming":
 			this.spriteCompressed = "16x48|010001A003C00AC0056005200AD007E005300BD00808118813C80240024004200420042004100000000000000000000000A000C005C0036002B00290056803F0029805E8040408C409E401200220022004200410041";
 			this.displayName = "Skelebones who is on fire";
-			this.color = "#ff5000"
+			this.shortName = "Flaming Skelebones"
+			this.color = "#ff5000";
 			break;
 		default:
 			this.spriteCompressed = "16x48|00000000000000000100010002C007E005300BD00808118813C802400240042004200420041000000000000000000000000000000000000000800080016003F0029805E8040408C409E401200220022004200410041";
@@ -110,6 +143,7 @@ Skele.prototype = new Monster();
 Skele.prototype.constructor = Skele;
 
 function Snek (type) {
+	this.initialHP = 10;
 	this.spriteCompressed = "16x48|000000000000000003C00FE008701FD010401F8008800F800480034021C040E043FC6FFE3FFC00000000000000000000000000000000000003C00FE008701FD010401F8008800F800480034081C040E043FC6FFE3FFC00000000000000000000";
 	this.displayName = "Snek";
 	this.color = 'green';
@@ -119,6 +153,7 @@ Snek.prototype.constructor = Snek;
 
 
 function Werebeing (type) {
+	this.initialHP = 20;
 	this.spriteCompressed = "16x48|0000000000800B0014001F803FE07FF01FF01FF817EC33C423CC31E423E007E00670023001080208040800000000000000000000000000800B0014001F803FE07FF01FF01FF817EC33C421EC33E427E006700230010802080408000000000000";
 	this.displayName = "Werebeing";
 	this.color = 'lightgray';
@@ -127,6 +162,7 @@ Werebeing.prototype = new Monster();
 Werebeing.prototype.constructor = Werebeing;
 
 function Mage (type) {
+	this.initialHP = 12;
 	this.spriteCompressed = "16x48|000000000000020002000F8002100D281F901FD02FD02738201045100D900A900DD01FD01FF03FF000000000000000000000000000000100010007C8011406880FC80FE817FC1388100825880D880A880DC81FC81FE83FF00000000000000000";
 	this.displayName = "Wiz";
 	this.color = 'blue';
